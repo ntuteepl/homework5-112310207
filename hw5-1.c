fbfb2600fb73dd9c623babc2028ef5a2b272bb93
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-31 15:20:26 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+  int main()
+{
+  double f;
+  float c;
+   scanf("%f",&c) ;
+    f = (c*9/5) + 32;
+    f = round (f*10)/10;
+   printf("%1f\n",f);
+    return 0;
+}
