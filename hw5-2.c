1332969c6cbba9f378706d48dddc122b117783d8
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-31 15:21:14 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+  int main()
+{
+  int min;
+  double tip;
+   scanf("%d",&min);
+    if(min <= 800 ){
+     tip=min *0.9;
+    }
+    if(800 <= min <=1500 ){
+     tip=(min *0.9) *(double)0.09;
+    }
+    if(1500<=min){
+     tip=((min *0.9) *(double)0.09) *(double)0.79;
+    }
+    printf("%fn",tip);
+}
+1分
