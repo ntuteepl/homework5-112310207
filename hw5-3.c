80a118cca4717682644a50e74494bad75419daac
112310207 <146065412+112310207@users.noreply.github.com>
2023-11-03 19:34:11 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-  int main()
-{
-  int w_h;
-  int h_r;
-  double s;
+  int main() {
 
-   scanf("%d%d",&w_h,&h_r);
-    if(w_h<=60){
-     s= w_h * h_r;
+  int w_h;  // 工时
+  int h_r;  // 时薪
+  double s; // 工资
+   scanf("%d %d", &w_h, &h_r);
+   if (w_h <= 60) {
+    s = w_h * h_r;
+}
+   else if (w_h <= 120) {
+    s =60 * h_r + (w_h - 60) * h_r * 1.33;
     }
-    if(61<=w_h && w_h<=120){
-     s= w_h * (h_r*1.33);
-    }
-    if(121<=w_h){
-     s= w_h * (h_r*1.66);
-     printf("%1.1f\n",s);
-    }
+   else {
+    s =60 * h_r + 60 * h_r * 1.33 + (w_h - 120) * h_r * 1.66;
+}
+  printf("%.1f\n", s);
+return 0;
 }
