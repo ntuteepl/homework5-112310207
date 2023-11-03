56bdf1a122e309a3616a1cd281606ef9a13edd8a
112310207 <146065412+112310207@users.noreply.github.com>
2023-11-03 15:50:48 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+  int main()
+{
+  int w_h;
+  int h_r;
+  double s;
+
+   scanf("%d%d",&w_h,&h_r);
+    if(w_h<=60){
+     s= w_h * h_r;
+    }
+    if(61<=w_h && w_h<=120){
+     s= w_h * (h_r*1.33);
+    }
+    if(121<=w_h){
+     s= w_h * (h_r*1.66);
+     printf("%1.1f\n",s);
+    }
+}
